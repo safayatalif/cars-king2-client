4bcb2414dce47a246dbbee9bfc1a83ce33205c20
safayatalif <safayatalif2021@gmail.com>
2023-05-18 15:18:59 +0600

    gallery and banner image added

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -8,25 +8,25 @@ const Banner = () => {
                 <div className="max-w-sm rounded-lg shadow-2xl">
                     <div className=" carousel rounded-box">
                         <div className="carousel-item w-full">
-                            <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
+                            <img src="https://img.freepik.com/free-photo/green-car-with-word-ford-front_1340-23250.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                         <div className="carousel-item w-full">
                             <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                         <div className="carousel-item w-full">
-                            <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
+                            <img src="https://img.freepik.com/premium-photo/3d-render-suv-car-greenish-cyan-color-3d-illustration-isolated-pastel-colors-minimal-scene_255268-474.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                         <div className="carousel-item w-full">
-                            <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
+                            <img src="https://img.freepik.com/free-vector/hand-drawn-transport-truck_23-2149161394.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=ais" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                         <div className="carousel-item w-full">
-                            <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
+                            <img src="https://img.freepik.com/free-vector/police-car_1308-35213.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=ais" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                         <div className="carousel-item w-full">
-                            <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
+                            <img src="https://img.freepik.com/free-vector/flat-ambulance_23-2147949454.jpg?size=626&ext=jpg" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                         <div className="carousel-item w-full">
-                            <img src="https://img.freepik.com/free-vector/modern-blue-urban-adventure-suv-vehicle-illustration_1344-205.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph" className="w-full" alt="Tailwind CSS Carousel component" />
+                            <img src="https://img.freepik.com/premium-vector/red-fire-truck-with-outline-vector-illustration-cartoon-childish-style-isolated-funny-clipart_456865-1940.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=ais" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div>
                     </div>
                 </div>
--- src/pages/home/gallery/Gallery.jsx
+++ src/pages/home/gallery/Gallery.jsx
@@ -6,70 +6,67 @@ const Gallery = () => {
     const photos = [
         {
             src: "https://img.freepik.com/free-vector/sticker-design-with-side-view-police-car-isolated_1308-76459.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais",
-            width: 800,
-            height: 600,
+            width: 300,
+            height: 200,
             srcSet: [
-                { src: "https://img.freepik.com/free-vector/sticker-design-with-side-view-police-car-isolated_1308-76459.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 400, height: 300 },
+                { src: "https://img.freepik.com/free-vector/sticker-design-with-side-view-police-car-isolated_1308-76459.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 300, height: 200 },
                 { src: "https://img.freepik.com/free-vector/sticker-design-with-side-view-police-car-isolated_1308-76459.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 200, height: 150 }
             ]
         },
         {
             src: "https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph",
-            width: 800,
-            height: 600,
+            width: 300,
+            height: 200,
             srcSet: [
-                { src: "https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 400, height: 300 },
+                { src: "https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 300, height: 200 },
                 { src: "https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 200, height: 150 }
             ]
         },
         {
             src: "https://img.freepik.com/premium-vector/cartoon-funny-blue-truck-character_29190-6777.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais",
-            width: 800,
-            height: 600,
+            width: 300,
+            height: 200,
             srcSet: [
-                { src: "https://img.freepik.com/premium-vector/cartoon-funny-blue-truck-character_29190-6777.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 400, height: 300 },
+                { src: "https://img.freepik.com/premium-vector/cartoon-funny-blue-truck-character_29190-6777.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 300, height: 200 },
                 { src: "https://img.freepik.com/premium-vector/cartoon-funny-blue-truck-character_29190-6777.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 200, height: 150 }
             ]
         },
         {
             src: "https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph",
-            width: 800,
-            height: 600,
+            width: 300,
+            height: 200,
             srcSet: [
-                { src: "https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 400, height: 300 },
+                { src: "https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 300, height: 200 },
                 { src: "https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 200, height: 150 }
             ]
         },
         {
             src: "https://img.freepik.com/premium-vector/children-toy-truck-cute-cartoon-illustration_204216-67.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais",
-            width: 800,
-            height: 600,
+            width: 300,
+            height: 200,
             srcSet: [
-                { src: "https://img.freepik.com/premium-vector/children-toy-truck-cute-cartoon-illustration_204216-67.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 400, height: 300 },
+                { src: "https://img.freepik.com/premium-vector/children-toy-truck-cute-cartoon-illustration_204216-67.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 300, height: 200 },
                 { src: "https://img.freepik.com/premium-vector/children-toy-truck-cute-cartoon-illustration_204216-67.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 200, height: 150 }
             ]
         },
         {
             src: "https://img.freepik.com/free-psd/3d-rendering-firefighter-icon_23-2149859768.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais",
-            width: 1600,
-            height: 900,
+            width: 300,
+            height: 200,
             srcSet: [
-                { src: "https://img.freepik.com/free-psd/3d-rendering-firefighter-icon_23-2149859768.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 800, height: 450 },
-                { src: "https://img.freepik.com/free-psd/3d-rendering-firefighter-icon_23-2149859768.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 400, height: 225 }
+                { src: "https://img.freepik.com/free-psd/3d-rendering-firefighter-icon_23-2149859768.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 300, height: 200 },
+                { src: "https://img.freepik.com/free-psd/3d-rendering-firefighter-icon_23-2149859768.jpg?size=626&ext=jpg&ga=GA1.2.1613183627.1673832056&semt=ais", width: 200, height: 150 }
             ]
         }
     ];
 
-    // const photos = [
-
-    //     { src: "https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 800, height: 600 },
-    //     { src: "https://img.freepik.com/free-photo/blue-black-muscle-car-with-license-plate-that-says-trans-front_1340-23399.jpg?size=626&ext=jpg&ga=GA1.1.1613183627.1673832056&semt=sph", width: 1600, height: 900 },
-    // ];
-
-
     return (
         <div className='bg-base-200 rounded-lg my-12 p-4'>
-            <PhotoAlbum layout="rows" breakpoints={[300, 1200]} photos={photos} />
+            <div className='text-center md:w-1/2 mx-auto py-8  space-y-4'>
+                <h1 className='text-3xl font-bold'>Photo Gallery</h1>
+                <p>Welcome to the Cars Toy Photo Gallery! In this collection, you will find a stunning array of toy cars that are sure to delight car enthusiasts of all ages. </p>
+            </div>
+            <PhotoAlbum layout="rows"  photos={photos} />
         </div>
     );
 };
